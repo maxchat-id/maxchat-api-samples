c9de4a6d3ddc38bb82f6a9c9511fb0466a25a169
misbah <misbah.qadri@gmail.com>
2024-07-10 06:54:43 +0700

    feat: add endpoint `/soap`

--- webhook-nodejs/index.js
+++ webhook-nodejs/index.js
@@ -1,16 +1,25 @@
-const polka = require('polka');
-const { json, urlencoded } = require('body-parser');
+const polka = require("polka");
+const { json, urlencoded, text } = require("body-parser");
 
 polka()
   .use(json(), urlencoded({ extended: true }))
-  .get('/', (_req, res) => {
-		res.writeHead(200, { 'Content-Type': 'application/json' });
-		res.end(JSON.stringify({ message: 'Gunakan post method' }));
+  .get("/", (_req, res) => {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Gunakan post method" }));
   })
-  .post('/', (req, res) => {
-    console.log(req.body)
-		res.writeHead(200, { 'Content-Type': 'application/json' });
-		res.end(JSON.stringify({ message: 'OK' }));
+  .post("/", (req, res) => {
+    console.log(req.body);
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "OK" }));
+  })
+  .use(text({ type: "application/soap+xml" }))
+  .post("/soap", (req, res) => {
+    console.log(req.body);
+
+    res.writeHead(200, { "Content-Type": "text/xml" });
+    res.end(
+      '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tns="http://www.example.com/SampleService"><soapenv:Body><bpjs:SampleResponse><output>OK</output></bpjs:SampleResponse></soapenv:Body></soapenv:Envelope>'
+    );
   })
   .listen(3000, () => {
     console.log(`> Running on localhost:3000`);
